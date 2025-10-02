1afdb3c1b25ce0e7009b5ba79e6ad4e6d2e47e38
ann-astrova <74923637+ann-astrova@users.noreply.github.com>
2025-10-02 14:46:45 +0300

    Add files via upload

---
+++ func_cooper.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const img = document.getElementById("cooper-img");
+
+  img.addEventListener("mouseover", () => {
+    img.style.opacity = "0"; // делаем прозрачным
+    setTimeout(() => {
+      img.src = "говард.png";
+      img.style.opacity = "1"; // плавно возвращаем
+    }, 300); // задержка меньше transition для синхронности
+  });
+
+  img.addEventListener("mouseout", () => {
+    img.style.opacity = "0";
+    setTimeout(() => {
+      img.src = "гуль.png";
+      img.style.opacity = "1";
+    }, 300);
+  });
+
+  const currentPage = window.location.pathname.split('/').pop();
+      const navLinks = document.querySelectorAll('.nav-link');
+
+      navLinks.forEach(link => {
+          const linkPage = link.getAttribute('href');
+          if (linkPage === currentPage) {
+              link.classList.add('active');
+          }
+
+          link.addEventListener('click', function(e) {
+              navLinks.forEach(l => l.classList.remove('active'));
+              this.classList.add('active');
+          });
+      });
+});
